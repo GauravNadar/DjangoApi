509aeafb7c6f147f51d6d775ccf57bbef7924b68
gaurav <nadargaurav@gmail.com>
2020-02-14 12:36:04 +0530

    change after migration success

--- rapidapipractice/urls.py
+++ rapidapipractice/urls.py
@@ -26,5 +26,5 @@ router.register(r'groups', GroupViewSet)
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api_auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api_auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
