8c8c83b95afe004cac563992b9ddc82296a2ec40
gaurav <nadargaurav@gmail.com>
2020-02-14 12:25:07 +0530

    offf changee

--- rapidapipractice/urls.py
+++ rapidapipractice/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from views import UserViewSet, GroupViewSet
+from api.views import UserViewSet, GroupViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
