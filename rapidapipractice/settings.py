7a4e592d21ac61d441d1dc38a98fa4dff91811b9
gaurav <nadargaurav@gmail.com>
2020-02-18 11:50:53 +0530

    h

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -135,9 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'api/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'api/static'),
+# )
 
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
