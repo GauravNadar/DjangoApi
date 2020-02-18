3682a41dbde704ad0b17405ad93ca4a93d93235f
gaurav <nadargaurav@gmail.com>
2020-02-18 10:53:05 +0530

    a

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -127,9 +127,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
