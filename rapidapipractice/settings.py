4fdabae468bd97b65dd43139e1df9101559a416d
gaurav-parinati <gaurav.nadar@parinati.in>
2020-02-26 12:21:22 +0530

    4th change

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -128,15 +128,10 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10,
-    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
-    ),
-    
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
-
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 # import dj_database_url
