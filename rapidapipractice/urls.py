eeed4702115cceb6a39a03d875d7236ece32009f
gaurav-parinati <gaurav.nadar@parinati.in>
2020-05-21 14:07:24 +0530

    fixed browsable auth

--- rapidapipractice/urls.py
+++ rapidapipractice/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import include
 from rest_framework import routers
 from api.views import UserViewSet, GroupViewSet, PersonViewSet, TestView, DatatableView, PersonDetailViewSet, TokenExample, SignalViewSet
 from rest_framework.authtoken import views as rest_auth_view
